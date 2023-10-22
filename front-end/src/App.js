93e5b3930babbda19a9f425c464aaa09631e6c95
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2023-10-22 16:03:22 +0200

    update style

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,20 +7,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Text Wise</h1>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
-      <h1>Text Wise</h1>
       <FileUpload />
       <Chat />
     </div>
